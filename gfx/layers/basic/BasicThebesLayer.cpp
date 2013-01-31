f2276436f13f6a23fd67c0eba065e3db97fd12dc
Jeff Muizelaar <jmuizelaar@mozilla.com>
2013-01-31 16:07:29 -0500

    Include BasicThebesTiledLayer.h to fix android build

--- gfx/layers/basic/BasicThebesLayer.cpp
+++ gfx/layers/basic/BasicThebesLayer.cpp
@@ -4,6 +4,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "BasicThebesLayer.h"
+#include "BasicTiledThebesLayer.h"
 #include "gfxUtils.h"
 #include "nsIWidget.h"
 #include "RenderTrace.h"
