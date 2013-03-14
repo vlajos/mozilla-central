9ea4c00b82029ecf7fb820293ae4954d372eb55f
Nicholas Cameron <ncameron@mozilla.com>
2013-03-15 06:53:35 +1300

    tidy up container layers

--- gfx/layers/composite/ContainerLayerComposite.cpp
+++ gfx/layers/composite/ContainerLayerComposite.cpp
@@ -12,6 +12,148 @@
 namespace mozilla {
 namespace layers {
 
+// HasOpaqueAncestorLayer and ContainerRender are shared between RefLayer and ContainerLayer
+static bool
+HasOpaqueAncestorLayer(Layer* aLayer)
+{
+  for (Layer* l = aLayer->GetParent(); l; l = l->GetParent()) {
+    if (l->GetContentFlags() & Layer::CONTENT_OPAQUE)
+      return true;
+  }
+  return false;
+}
+
+template<class ContainerT> static void
+ContainerRender(ContainerT* aContainer,
+                CompositingRenderTarget* aPreviousTarget,
+                const nsIntPoint& aOffset,
+                LayerManagerComposite* aManager,
+                const nsIntRect& aClipRect)
+{
+  /**
+   * Setup our temporary surface for rendering the contents of this container.
+   */
+  RefPtr<CompositingRenderTarget> surface;
+
+  Compositor* compositor = aManager->GetCompositor();
+
+  nsIntPoint childOffset(aOffset);
+  nsIntRect visibleRect = aContainer->GetEffectiveVisibleRegion().GetBounds();
+
+  nsIntRect cachedScissor = aClipRect;
+  aContainer->mSupportsComponentAlphaChildren = false;
+
+  float opacity = aContainer->GetEffectiveOpacity();
+  bool needsSurface = aContainer->UseIntermediateSurface();
+  if (needsSurface) {
+    SurfaceInitMode mode = INIT_MODE_CLEAR;
+    bool surfaceCopyNeeded = false;
+    gfx::IntRect surfaceRect = gfx::IntRect(visibleRect.x, visibleRect.y, visibleRect.width,
+                                            visibleRect.height);
+    // we're about to create a framebuffer backed by textures to use as an intermediate
+    // surface. What to do if its size (as given by framebufferRect) would exceed the
+    // maximum texture size supported by the GL? The present code chooses the compromise
+    // of just clamping the framebuffer's size to the max supported size.
+    // This gives us a lower resolution rendering of the intermediate surface (children layers).
+    // See bug 827170 for a discussion.
+    int32_t maxTextureSize = compositor->GetMaxTextureSize();
+    surfaceRect.width = std::min(maxTextureSize, surfaceRect.width);
+    surfaceRect.height = std::min(maxTextureSize, surfaceRect.height);
+    if (aContainer->GetEffectiveVisibleRegion().GetNumRects() == 1 && 
+        (aContainer->GetContentFlags() & Layer::CONTENT_OPAQUE))
+    {
+      // don't need a background, we're going to paint all opaque stuff
+      aContainer->mSupportsComponentAlphaChildren = true;
+      mode = INIT_MODE_NONE;
+    } else {
+      const gfx3DMatrix& transform3D = aContainer->GetEffectiveTransform();
+      gfxMatrix transform;
+      // If we have an opaque ancestor layer, then we can be sure that
+      // all the pixels we draw into are either opaque already or will be
+      // covered by something opaque. Otherwise copying up the background is
+      // not safe.
+      if (HasOpaqueAncestorLayer(aContainer) &&
+          transform3D.Is2D(&transform) && !transform.HasNonIntegerTranslation()) {
+        mode = gfxPlatform::GetPlatform()->UsesSubpixelAATextRendering() ?
+                                            INIT_MODE_COPY : INIT_MODE_CLEAR;
+        surfaceCopyNeeded = (mode == INIT_MODE_COPY);
+        surfaceRect.x += transform.x0;
+        surfaceRect.y += transform.y0;
+        aContainer->mSupportsComponentAlphaChildren
+          = gfxPlatform::GetPlatform()->UsesSubpixelAATextRendering();
+      }
+    }
+
+    aManager->SaveViewport();
+    surfaceRect -= gfx::IntPoint(childOffset.x, childOffset.y);
+    if (!aManager->CompositingDisabled()) {
+      if (surfaceCopyNeeded) {
+        surface = compositor->CreateRenderTargetFromSource(surfaceRect, aPreviousTarget);
+      } else {
+        surface = compositor->CreateRenderTarget(surfaceRect, mode);
+      }
+      compositor->SetRenderTarget(surface);
+    }
+    childOffset.x = visibleRect.x;
+    childOffset.y = visibleRect.y;
+  } else {
+    surface = aPreviousTarget;
+    aContainer->mSupportsComponentAlphaChildren = (aContainer->GetContentFlags() & Layer::CONTENT_OPAQUE) ||
+      (aContainer->GetParent() && aContainer->GetParent()->SupportsComponentAlphaChildren());
+  }
+
+  nsAutoTArray<Layer*, 12> children;
+  aContainer->SortChildrenBy3DZOrder(children);
+
+  /**
+   * Render this container's contents.
+   */
+  for (uint32_t i = 0; i < children.Length(); i++) {
+    LayerComposite* layerToRender = static_cast<LayerComposite*>(children.ElementAt(i)->ImplData());
+
+    if (layerToRender->GetLayer()->GetEffectiveVisibleRegion().IsEmpty()) {
+      continue;
+    }
+
+    nsIntRect scissorRect = layerToRender->GetLayer()->
+        CalculateScissorRect(cachedScissor, &aManager->GetWorldTransform());
+    if (scissorRect.IsEmpty()) {
+      continue;
+    }
+
+    layerToRender->RenderLayer(childOffset, scissorRect, surface);
+    // invariant: our GL context should be current here, I don't think we can
+    // assert it though
+  }
+
+  if (needsSurface) {
+    // Unbind the current surface and rebind the previous one.
+    compositor->SetRenderTarget(aPreviousTarget);
+#ifdef MOZ_DUMP_PAINTING
+    if (gfxUtils::sDumpPainting) {
+      nsRefPtr<gfxImageSurface> surf = surface->Dump(aManager->GetCompositor());
+      WriteSnapshotToDumpFile(aContainer, surf);
+    }
+#endif
+
+    aManager->RestoreViewport();
+    if (!aManager->CompositingDisabled()) {
+      EffectChain effectChain;
+      LayerManagerComposite::AddMaskEffect(aContainer->GetMaskLayer(),
+                                           effectChain,
+                                           !aContainer->GetTransform().CanDraw2D());
+
+      effectChain.mPrimaryEffect = new EffectRenderTarget(surface);
+      gfx::Matrix4x4 transform;
+      ToMatrix4x4(aContainer->GetEffectiveTransform(), transform);
+
+      gfx::Rect rect(visibleRect.x, visibleRect.y, visibleRect.width, visibleRect.height);
+      aManager->GetCompositor()->DrawQuad(rect, nullptr, effectChain, opacity,
+                                          transform, gfx::Point(aOffset.x, aOffset.y));
+    }
+  }
+}
+
 ContainerLayerComposite::ContainerLayerComposite(LayerManagerComposite *aManager)
   : ShadowContainerLayer(aManager, nullptr)
   , LayerComposite(aManager)
@@ -34,26 +176,89 @@ ContainerLayerComposite::~ContainerLayerComposite()
   // *ContainerLayerOGL::Destroy(), or Disconnect() will trigger
   // cleanup of our resources.
   while (mFirstChild) {
-    ContainerRemoveChild(this, mFirstChild);
+    RemoveChild(mFirstChild);
   }
 }
 
 void
 ContainerLayerComposite::InsertAfter(Layer* aChild, Layer* aAfter)
 {
-  ContainerInsertAfter(this, aChild, aAfter);
+  NS_ASSERTION(aChild->Manager() == Manager(),
+               "Child has wrong manager");
+  NS_ASSERTION(!aChild->GetParent(),
+               "aChild already in the tree");
+  NS_ASSERTION(!aChild->GetNextSibling() && !aChild->GetPrevSibling(),
+               "aChild already has siblings?");
+  NS_ASSERTION(!aAfter ||
+               (aAfter->Manager() == Manager() &&
+                aAfter->GetParent() == this),
+               "aAfter is not our child");
+
+  aChild->SetParent(this);
+  if (aAfter == mLastChild) {
+    mLastChild = aChild;
+  }
+  if (!aAfter) {
+    aChild->SetNextSibling(mFirstChild);
+    if (mFirstChild) {
+      mFirstChild->SetPrevSibling(aChild);
+    }
+    mFirstChild = aChild;
+    NS_ADDREF(aChild);
+    DidInsertChild(aChild);
+    return;
+  }
+
+  Layer* next = aAfter->GetNextSibling();
+  aChild->SetNextSibling(next);
+  aChild->SetPrevSibling(aAfter);
+  if (next) {
+    next->SetPrevSibling(aChild);
+  }
+  aAfter->SetNextSibling(aChild);
+  NS_ADDREF(aChild);
+  DidInsertChild(aChild);  
 }
 
 void
 ContainerLayerComposite::RemoveChild(Layer *aChild)
 {
-  ContainerRemoveChild(this, aChild);
+  NS_ASSERTION(aChild->Manager() == Manager(),
+               "Child has wrong manager");
+  NS_ASSERTION(aChild->GetParent() == this,
+               "aChild not our child");
+
+  Layer* prev = aChild->GetPrevSibling();
+  Layer* next = aChild->GetNextSibling();
+  if (prev) {
+    prev->SetNextSibling(next);
+  } else {
+    this->mFirstChild = next;
+  }
+  if (next) {
+    next->SetPrevSibling(prev);
+  } else {
+    this->mLastChild = prev;
+  }
+
+  aChild->SetNextSibling(nullptr);
+  aChild->SetPrevSibling(nullptr);
+  aChild->SetParent(nullptr);
+
+  this->DidRemoveChild(aChild);
+  NS_RELEASE(aChild);  
 }
 
 void
 ContainerLayerComposite::Destroy()
 {
-  ContainerDestroy(this);
+  if (!mDestroyed) {
+    while (mFirstChild) {
+      static_cast<LayerComposite*>(GetFirstChild()->ImplData())->Destroy();
+      RemoveChild(mFirstChild);
+    }
+    mDestroyed = true;
+  }
 }
 
 LayerComposite*
@@ -68,7 +273,46 @@ ContainerLayerComposite::GetFirstChildComposite()
 void
 ContainerLayerComposite::RepositionChild(Layer* aChild, Layer* aAfter)
 {
-  ContainerRepositionChild(this, aChild, aAfter);
+  NS_ASSERTION(aChild->Manager() == Manager(),
+               "Child has wrong manager");
+  NS_ASSERTION(aChild->GetParent() == this,
+               "aChild not our child");
+  NS_ASSERTION(!aAfter ||
+               (aAfter->Manager() == Manager() &&
+                aAfter->GetParent() == this),
+               "aAfter is not our child");
+
+  Layer* prev = aChild->GetPrevSibling();
+  Layer* next = aChild->GetNextSibling();
+  if (prev == aAfter) {
+    // aChild is already in the correct position, nothing to do.
+    return;
+  }
+  if (prev) {
+    prev->SetNextSibling(next);
+  }
+  if (next) {
+    next->SetPrevSibling(prev);
+  }
+  if (!aAfter) {
+    aChild->SetPrevSibling(nullptr);
+    aChild->SetNextSibling(mFirstChild);
+    if (mFirstChild) {
+      mFirstChild->SetPrevSibling(aChild);
+    }
+    mFirstChild = aChild;
+    return;
+  }
+
+  Layer* afterNext = aAfter->GetNextSibling();
+  if (afterNext) {
+    afterNext->SetPrevSibling(aChild);
+  } else {
+    mLastChild = aChild;
+  }
+  aAfter->SetNextSibling(aChild);
+  aChild->SetPrevSibling(aAfter);
+  aChild->SetNextSibling(afterNext);  
 }
 
 void
@@ -82,7 +326,10 @@ ContainerLayerComposite::RenderLayer(const nsIntPoint& aOffset,
 void
 ContainerLayerComposite::CleanupResources()
 {
-  ContainerCleanupResources(this);
+  for (Layer* l = GetFirstChild(); l; l = l->GetNextSibling()) {
+    LayerComposite* layerToCleanup = static_cast<LayerComposite*>(l->ImplData());
+    layerToCleanup->CleanupResources();
+  }
 }
 
 RefLayerComposite::RefLayerComposite(LayerManagerComposite* aManager)
